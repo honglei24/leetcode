14045f886ea0ffd6a02893a4f9b19373be9c8a83
honglei24 <shaka202@gmail.com>
2020-03-15 22:19:15 +0800

    add lengthOfLongestSubstring.go

---
+++ src/go/lengthOfLongestSubstring.go
@@ -0,0 +1,24 @@
+func lengthOfLongestSubstring(s string) int {
+	max := 0
+	tmp := 0
+	m := make(map[int32]int)
+	for i, v := range s {
+		if value, ok := m[v]; ok {
+			if tmp > max {
+				max = tmp
+			}
+			for j := i - tmp; j <= value; j++ {
+				delete(m, int32(s[j]))
+			}
+			tmp = i - value
+
+		} else {
+			tmp = tmp + 1
+		}
+		m[v] = i
+	}
+	if tmp > max {
+		max = tmp
+	}
+	return max
+}
